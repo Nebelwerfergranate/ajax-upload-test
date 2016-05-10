352b71efcdb07ce945c50f89ed476fe2c4b2aa13
Vitalii <acht-acht@mail.ru>
2016-05-10 15:34:30 +0300

    Add project files.

---
+++ AjaxUpload/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AjaxUpload.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ AjaxUpload/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AjaxUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return View();
+            return RedirectToAction("AdvancedTest");
+        }
+
+        [HttpGet]
+        public ActionResult Test() {
+            return View();
+        }
+        
+        public ActionResult AdvancedTest()
+        {
+            return View();
+        } 
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            foreach (string file in Request.Files)
+            {
+                var upload = Request.Files[file];
+                if(upload != null)
+                {
+                    // getting file name
+                    string fileName = System.IO.Path.GetFileName(upload.FileName);
+                    // saving file in a Files folder in project.
+
+                    upload.SaveAs(Server.MapPath("~\\Files\\" + fileName));
+                   
+                }
+            }
+
+            return Json("файл загружен");
+        }
+    }
+}
